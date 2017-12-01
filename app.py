501cbc2da9ec9e5d43f7de59441ce55e0a886caa
Matthewpdias <matthewpdias@gmail.com>
2017-12-01 03:17:09 +0000

    about to start remote editing, vim sucks

--- app.py
+++ app.py
@@ -1,26 +1,46 @@
 from flask import Flask, render_template
+from flask_script import Manager, Server
 from flask_pymongo import PyMongo
 import datetime
 import threading
 
+class MyServer(Server):
+  def __call__(self, app, *args, **kwargs):
+    poll_and_insert_temps()
+    return Server.__call__(self, app, *args, **kwargs)
+
 app = Flask(__name__)
+manager = Manager(app)
 mongo = PyMongo(app)
 
-@app.route('/')
-def run():
-  return render_template('index.html', temps=get_temp())
+manager.add_command('runserver', MyServer())
 
-def get_temp():
-  threading.Timer(5.0, get_temp).start()
-  with file('/sys/bus/w1/devices/28-0516a4123aff/w1_slave') as f:
+
+if __name__ == "__main__":
+  manager.run()
+
+@app.route('/')
+def home():
+  ##poll the sensor and write the value to mongo
+  s = ""
+  with open('/sys/bus/w1/devices/28-0516a4123aff/w1_slave') as f:
      s = f.read().split("\n")[1].split(" ")[9].split("=")[1]
   temp = int(s)/1000.0*9.0/5.0+32
-  temp = 98.3
-
-
   timestamp = datetime.datetime.utcnow()
-  
-  temperature = {'timestamp' : timestamp, 'temp' : temp}
+  temperature = {'timestamp': timestamp, 'temp': temp}
+ 
+  print("inserting into db:")
+  print(temperature)
   mongo.db.temperature.insert_one(temperature)
 
-  return temperatures
+  ##get values to output
+  print("reading from db:")
+
+  temps = mongo.db.teperature.find( {} ).limit(10)
+  for entry in temps:
+    
+
+  print(temps)
+
+  return render_template('index.html', temps=temps)
+
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #sudo modprobe w1-gpio
 #sudo modprobe w1-therm
-export FLASK_APP=app.py && flask run
+export FLASK_APP=app.py && flask run --host=0.0.0.0
